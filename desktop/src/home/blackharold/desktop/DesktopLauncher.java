64e9f630427b84933fdda3b9169b188cbe7b700d
BlackHarold <info@teh-dozor.ru>
2018-09-07 08:26:34 +0300

    Initial commit

---
+++ desktop/src/home/blackharold/desktop/DesktopLauncher.java
@@ -0,0 +1,19 @@
+package home.blackharold.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import home.blackharold.Drop;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Drop";
+        config.width = 800;
+        config.height = 480;
+
+        new LwjglApplication(new Drop(), config);
+
+
+    }
+}
